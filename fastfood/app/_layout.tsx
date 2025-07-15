d4d0ad6d31e5f62c28f205beef97da95711aec56
Omneesh9837 <omneesh7@gmail.com>
2025-07-15 08:49:48 +0000

    add assests

--- fastfood/app/_layout.tsx
+++ fastfood/app/_layout.tsx
@@ -1,6 +1,47 @@
-import { Stack } from "expo-router";
+/*import { SplashScreen, Stack } from "expo-router";
 import './global.css';
+import {useFonts} from 'expo-font';
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  const[fontsLoaded,error]=useFonts(map:{
+    "Quicksand-Bold":require(id:'../assets/fonts/Quicksand-Bold.ttf'),
+    "Quicksand-Medium":require(id:'../assets/fonts/Quicksand-Medium.ttf'),
+    "Quicksand-Regular":require(id:'../assets/fonts/Quicksand-Regular.ttf'),
+    "Quicksand-Light":require(id:'../assets/fonts/Quicksand-Light.ttf'),
+    "Quicksand-SemiBold":require(id:'../assets/fonts/Quicksand-SemiBold.ttf'),
+  });
+  useEffect(effect:()=>{
+    if(error)throw error
+    if(fontsLoaded)SplashScreen.hideAsync();
+
+  },deps:[fontsLoaded,error]);
+  return <Stack  screenOptions={{headerShown:false}}/>;
+}
+*/
+import { SplashScreen, Stack } from "expo-router";
+import "./global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  // Show nothing until fonts are loaded
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- fastfood/app/index.tsx
+++ fastfood/app/index.tsx
@@ -4,8 +4,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-bold text-blue-500">
+        Welcome to my Nativewind!
       </Text>
     </View>
   );
