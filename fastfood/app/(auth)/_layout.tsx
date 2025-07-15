6b347eccffb79ae20fe3a0b3ae5876758e0905c7
Omneesh9837 <omneesh7@gmail.com>
2025-07-15 12:18:57 +0000

    components

--- fastfood/app/(auth)/_layout.tsx
+++ fastfood/app/(auth)/_layout.tsx
@@ -1,12 +1,19 @@
-import {Text,View} from 'react-native'
+import {Text,View,KeyboardAvoidingView,Platform,Dimensions,ScrollView,ImageBackground,Image} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context"
 import {Slot} from "expo-router"
 export default function _Layout(){
     return(
-        <SafeAreaView>
-            <Text>Auth Layout</Text>
+        <KeyboardAvoidingView behavior={Platform.OS==='ios'?'padding':'height'}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                <View className="w-full relative" style={{height:Dimensions.get('screen').height/2.25}}>
+                <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch/>
+                <Image source={images.logo} className="self-center size-48 absolute style={{ bottom: -64 }} />
+
+                </View>
+            </ScrollView>
+           
             <Slot/>
-        </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
---
+++ fastfood/lib/appwrite.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig={
+    endpoint:process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform:"com.niche.foodorder",
+    databaseId:'68764035001e08605e89',
+    userCollectionId:'6876407d0015e2e4859a'
+    
+}
